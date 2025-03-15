b023798799b8e3e3926031f51cf6ce47374535a1
kiel <1692335726@qq.com>
2025-03-15 15:46:18 +0800

    八股

--- StudyCpp.cpp
+++ StudyCpp.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 #include "StudyCpp.h"
+#include <vector>
 using namespace std;
+/*在C++中，std是一个命名空间（namespace），它包含了C++标准库的所有功能。
+ *C++标准库提供了许多有用的组件，包括输入输出、字符串处理、容器（如向量、列表、集合等）、算法（如排序、查找等）、以及其他辅助功能。*/
+
+extern int a = 0;  //声明a是一个全局变量，但不会分配内存
+//使用自定义的命名空间
 namespace MyNameSpace
 {
     int value = 42;
 }
+
 int main(int argc, char* argv[])
 {
-    unique_ptr<UStudyCpp> StudyCppPtr = make_unique<UStudyCpp>();  //创建一个唯一指针指向唯一的头文件类
+    unique_ptr<UStudyCpp> StudyCppPtr = make_unique<UStudyCpp>();  //创建一个唯一指针指向唯一的头文件类，显式分配内存
     StudyCppPtr->UniquePtrTestFunc();
     StudyCppPtr->SharedPtrTestFunc();
     StudyCppPtr->WeakPtrTestFunc();
-    cout<<MyNameSpace::value<<'\n'; //输出命名空间的语句  这是作用域解析的一种，作用域是为了防止访问名称冲突，提高代码可读性
-    Outer::Inner InnerObject;  //声明对象实例，内存分配在栈区，在编译时分配，创建类中定义的嵌套类，用作用域阐述
-    InnerObject.display();
-
-    TestClass TestClass01;
-    TestClass01.TestNum = 4;
-    cout << TestClass01.TestNum << '\n';
-
-    TestClass TestClass02;
-    TestClass02.TestNum = 5;
-    cout << TestClass02.TestNum << '\n';
-
-    
+    StudyCppPtr->TestClassObject();
+    StudyCppPtr->OperatorTest();
+    StudyCppPtr->PointerArrayTest();
+    StudyCppPtr->MergeStringTest();
+    Singleton::GetInstance()->doSomething();
+    Singleton::GetInstance()->ClearPtr();
     return 0;
 }
 
@@ -64,3 +64,91 @@ void UStudyCpp::WeakPtrTestFunc()
 
    // 结束时，PtrB 和 PtrA 的引用计数各减 1 → 均为 1，对象无法释放！ 使用弱指针，类内部的成员就不会在共享的时候增加引用计数，确保两个类被正常析构
 }
+
+void UStudyCpp::TestClassObject()
+{
+    cout<<MyNameSpace::value<<'\n'; //输出命名空间的语句  这是作用域解析的一种，作用域是为了防止访问名称冲突，提高代码可读性
+    Outer::Inner InnerObject;  //声明对象实例，内存分配在栈区，在编译时分配，创建类中定义的嵌套类，用作用域阐述
+    InnerObject.display();
+    
+    TestClass TestClass01;
+    TestClass01.TestNum = 4;
+    cout << TestClass01.TestNum << '\n';
+
+    TestClass TestClass02;
+    TestClass02.TestNum = 5;
+    cout << TestClass02.TestNum << '\n';
+}
+
+//基础运算符重载
+void UStudyCpp::OperatorTest()
+{
+    Complex TestA(3,5);
+    Complex TestB(5,3);
+    cout<<"重载运算符输出结果是："<<TestA + TestB<<'\n';
+    if(TestA == TestB)
+    {
+        cout<<"比较运算符输出结果是：True"<<'\n';
+    }
+    else
+    {
+        cout<<"比较运算符输出结果是：False"<<'\n';
+    }
+
+    OutAndIn OI(5);
+    cout<<"输出流输出的结果为："<<OI<<'\n';
+}
+
+//指针数组 vs 数组指针
+void UStudyCpp::PointerArrayTest()
+{
+    int a = 5;
+    int (*Ptr)[3];
+    int data[3] = {1,2,3};
+    Ptr = &data;
+    cout<<*Ptr[1]<<'\n'; //数组指针，指向一块数组地址的指针
+
+    int* NewPtr = &a;
+    vector<int*> NewVec;
+    NewVec.push_back(NewPtr);
+    for(auto it = NewVec.begin(); it < NewVec.end(); ++it)
+    {
+        cout<<*(*it)<<'\n';  //指针指向地址，然后又解引用
+    }  //指针数组，包含指针的数组
+
+    //指针算数运算，遍历，使用Vector容器
+    vector<int> vec{1, 2, 3};
+    for (auto it = vec.begin(); it != vec.end(); ++it) {
+        cout << *it << "\n"; // 输出 1 2 3
+    }
+}
+
+//字符串测试
+void UStudyCpp::MergeStringTest()
+{
+    string s1 = "Hello";
+    string s2 = "World";
+    string s3 = s1 + s2;
+    cout<<s3<<'\n';
+    size_t pos =  s1.find("Hel");
+    cout<<"找到的字符串的位置"<<pos<<'\n';  //查找对应字符串的起始位置
+
+    string path = "/home/user";
+    size_t pos2  = path.find_last_of('/');  //截取第一个/后面的字符
+    if(pos2 != string::npos)
+    {
+        string filename = path.substr(pos + 1);
+        cout<<"提取出的文档 ： "<<filename<<'\n';
+    }
+
+    string message = "I like apples";
+    message.replace(7,6,"oranges");  //从7号开始替换六个字符
+    cout<<"替换的字符为："<<message<<'\n';
+
+    std::string data;
+    data.reserve(1000); // 预分配足够内存，避免多次扩容
+    for (int i = 0; i < 1000; ++i) {
+        data += "x"; // 追加字符时不会触发扩容
+    }
+}
+
--- StudyCpp.h
+++ StudyCpp.h
@@ -2,6 +2,14 @@
 #include "UniquePtrTest.h"
 #include "SharedPtrTest.h"
 #include "WeakPtrTest.h"
+//强类型枚举
+enum class Fruit
+{
+    None = 0,
+    Apple = 1,
+    Banana = 2,
+    Cherry = 3
+};
 class UStudyCpp
 {
 public:
@@ -13,9 +21,14 @@ public:
     void UniquePtrTestFunc();
     void SharedPtrTestFunc();
     void WeakPtrTestFunc();
+    void TestClassObject();
+    void OperatorTest();
+    void PointerArrayTest();
+    void MergeStringTest();
 
     unique_ptr<UniquePtrTest> TestUniquePtr;
     shared_ptr<SharedPtrTest> TestSharedPtr;
+    Fruit AnyFruit = Fruit::None;  //强类型枚举
 };
 class Outer
 {
@@ -30,4 +43,62 @@ class TestClass
 {
 public:
     int TestNum = 0;
+};
+
+class Complex
+{
+
+public:
+    int UseA = 3;
+    int UseB = 5;
+    Complex(int A,int B) : UseA(A), UseB(B) {}
+    //自定义运算符重载
+    int operator+(const Complex& OtherTestClass)
+    {
+        return (UseA + OtherTestClass.UseB);
+    }
+};
+inline bool operator==(const Complex& OtherTestClass , const Complex& OtherTestClass2)
+{
+    return OtherTestClass.UseA == OtherTestClass2.UseA && OtherTestClass.UseB == OtherTestClass2.UseB;
+}
+class OutAndIn
+{
+public:
+    int value = 0;
+    OutAndIn(int InValue) : value(InValue) {}
+
+    friend ostream& operator<<(ostream& os , const OutAndIn& OtherClass)
+    {
+        return os << OtherClass.value;
+    }
+};
+
+//创建单例模式，全局唯一单例，避免一个类被创建多个对象然后做不同的访问和修改
+//应用：配置文件时创建全局唯一配置源，防止多个实例读取和修改不同的配置
+class Singleton
+{
+public:
+    static Singleton* GetInstance()
+    {
+        static Singleton* instance = new Singleton;  //显式创建
+        return instance;  //获取全局唯一单例
+    }
+    void doSomething()
+    {
+        cout<<"I am something"<<'\n';
+    }
+    void ClearPtr()
+    {
+        delete this;
+    }
+    ~Singleton()
+    {
+        cout<<"I am the destructor"<<'\n';
+    }
+private:
+    Singleton() {}
+    Singleton(const Singleton&) = delete;  //禁用拷贝构造
+    Singleton& operator=(const Singleton&) = delete;  //禁用赋值运算符
+    //删除了以后就不能创建对象了，防止在访问静态函数的时候有多个成员访问这种情况
 };
